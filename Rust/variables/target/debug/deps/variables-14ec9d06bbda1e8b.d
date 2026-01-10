936632d05265e33d30cbb32106e28bd3872188a6
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-10 21:07:00 +0000

    task_rust: variables_wrong

---
+++ Rust/variables/target/debug/deps/variables-14ec9d06bbda1e8b.d
@@ -0,0 +1,5 @@
+/Users/ekete/Documents/Dev/low-level/Rust/variables/target/debug/deps/variables-14ec9d06bbda1e8b.d: src/main.rs
+
+/Users/ekete/Documents/Dev/low-level/Rust/variables/target/debug/deps/variables-14ec9d06bbda1e8b: src/main.rs
+
+src/main.rs:
