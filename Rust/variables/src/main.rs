f23adbb4e0aa2ca4045c874c48180cb780544c9a
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-12 21:07:00 +0000

    task_rust: variables_mutability

--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x = 5;
-    println!("the value of x is: {x}");
+    let mut x = 5;
+    println!("the value of x is: {}", x);
     x = 6;
     println!("the value of x is: {x}");
 }
---
+++ Rust/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/ekete/Documents/Dev/low-level/Rust/variables/target/debug/variables: /Users/ekete/Documents/Dev/low-level/Rust/variables/src/main.rs
