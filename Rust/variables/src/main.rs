5d37dd95c417417b83f6faab96219c94bebd5c9e
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-13 21:07:00 +0000

    task_rust: variables_shadowing

--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -1,6 +1,16 @@
-fn main() {
-    let mut x = 5;
-    println!("the value of x is: {}", x);
-    x = 6;
+// fn main() {
+//     let mut x = 5;
+//     println!("the value of x is: {}", x);
+//     x = 6;
+//     println!("the value of x is: {x}");
+// }
+
+fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("the value of x is: {x}");
+    }
     println!("the value of x is: {x}");
 }
