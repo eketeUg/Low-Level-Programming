4ba7d21123e64541ad9a84a5f71c3b0ed8e1e047
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-17 21:07:00 +0000

    task_rust: numeric operation

---
+++ Rust/numeric_operation/target/debug/deps/numeric_operation-ad50da856e91e80a.d
@@ -0,0 +1,5 @@
+/Users/ekete/Documents/Dev/low-level/Rust/numeric_operation/target/debug/deps/numeric_operation-ad50da856e91e80a.d: src/main.rs
+
+/Users/ekete/Documents/Dev/low-level/Rust/numeric_operation/target/debug/deps/numeric_operation-ad50da856e91e80a: src/main.rs
+
+src/main.rs:
---
+++ Rust/numeric_operation/target/debug/numeric_operation.d
@@ -0,0 +1,1 @@
+/Users/ekete/Documents/Dev/low-level/Rust/numeric_operation/target/debug/numeric_operation: /Users/ekete/Documents/Dev/low-level/Rust/numeric_operation/src/main.rs
