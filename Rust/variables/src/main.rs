03d21214f213e187a794b973f4309aef7db0dec7
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-01-19 21:07:00 +0000

    task_rust: data types

--- Rust/variables/src/main.rs
+++ Rust/variables/src/main.rs
@@ -23,8 +23,29 @@
 
 // integer type
 
-fn main() {
-    let x = 2.0; // f64
+// fn main() {
+//     let x = 2.0; // f64
 
-    let y: f32 = 3.0; // f32
+//     let y: f32 = 3.0; // f32
+// }
+
+
+// fn main() {
+//     let c = 'z';
+//     let z: char = 'ℤ'; // with explicit type annotation
+//     let heart_eyed_cat = '😻';
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+// }
+
+// The variable tup binds to the entire tuple because a tuple is considered a single compound element. To get the individual values out of a tuple, we can use pattern matching to destructure a tuple value, like this:
+
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
 }
